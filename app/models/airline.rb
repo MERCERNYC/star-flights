bed42fc225295c8623229b10ca996fd02f601e2f
Juliana Fogg <julianafogg@gmail.comgit config --global user.name Juliana>
2021-04-27 16:03:26 -0400

    initial commit

---
+++ app/models/airline.rb
@@ -0,0 +1,18 @@
+class Airline < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify
+
+    #parameterize method should handle both downcasing characters and replacing spaces with hyphens 
+    def slugify
+      self.slug = name.parameterize  
+    end
+
+    # avg_score method should take all of the reviews that belong to an airline and get the average overall rating
+
+    def avg_score
+      return 
+      reviews.average(:score).round(2).to_f
+    end 
+
+end

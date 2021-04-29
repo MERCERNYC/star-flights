e6b903302771dce3422424790d3ed7146883f584
Juliana Fogg <julianafogg@gmail.comgit config --global user.name Juliana>
2021-04-29 13:32:12 -0400

    add actions to reviews_controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,29 @@
 module API
     module V1
       class ReviewsController < ApplicationController
+
+      def create
+        review = Review.new(reviews_params)
+
+        if review.save
+          render json: { error: review.errors.messages }, status: 422
       end 
+
+      def destroy
+        review = Review.find(params[:id])
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: review.errors.messages }, status: 422
+        end 
+      end
+
+      private
+
+      def reviews_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end 
+      
     end 
   end 
+end 

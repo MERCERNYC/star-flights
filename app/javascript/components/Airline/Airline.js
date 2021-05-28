2ba52baf3be99f39a7028c1d3c180d33b4f944ba
Juliana Fogg <julianafogg@gmail.comgit config --global user.name Juliana>
2021-05-28 14:18:02 -0400

    add style to airline

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,12 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import Header from './Header'
+import Header from './Header';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display-grid: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+  background: #fff; 
+  max-width: 50%;
+  width: 50%;
+  float: left; 
+  height: 100vh;
+  overflow-x: scroll;
+  overflow-y: scroll; 
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  &:last-child {
+    background: black;
+    border-top: 1px solid rgba(255,255,255,0.5);
+  }
+`
+
+const Main = styled.div`
+  padding-left: 60px;
+`
 
 
 const Airline = (props) => {
-  const [ airline, setAirline] = useState({});
-  const [ review, setReviews] = useState({});
-  const [loaded, setLoaded] = useState(false)
+  const [ airline, setAirline ] = useState({});
+  const [ review, setReviews ] = useState({});
+  const [ loaded, setLoaded ] = useState(false);
 
   useEffect(() => {
     const slug = props.match.params.slug
@@ -20,23 +49,32 @@ const Airline = (props) => {
   }, [])
 
   return (
-    <div className='wrapper'>
-      <div className='column'>
-        {
-          loaded && 
-          <Header 
-            attributes={airline.data.attributes}
-            reviews={airline.included}
-          />
-        }
-          <div className='reviews'></div>
-        </div>
-        <div className='column'>
-          <div className='review-form'></div>
-        </div>
-      </div>
-    
-  )
-};
+    <Wrapper>
+      { 
+        loaded &&
+        <Fragment>
+          <Column>
+            <Main>
+              <Header 
+                attributes={airline.data.attributes}
+                reviews={airline.included}
+              />
 
+            </Main>
+          </Column>
+          <Column>
+            {/* <ReviewForm
+              // name={airline.data.attributes.name}
+              // review={review}
+              // handleChange={handleChange}
+              // handleSubmit={handleSubmit}
+              // setRating={setRating}
+              // error={error}
+            /> */}
+          </Column>
+        </Fragment>
+      }
+    </Wrapper>
+  )
+}
 export default  Airline;
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0px;
+  font-size:30px;
+  img {
+    margin-right: 10px;
+    height: 60px;
+    width: 60px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 100%;
+    margin-bottom: -8px;
+  }
+`
+const TotalReviews = styled.div`
+   font-size: 18px;
+   padding: 10px 0;
+`
+
+const TotalOutOf = styled.div`
+  padding-top: 12px;
+  font-size: 18px;
+  font-weight: bold;
+`
 
 const Header = (props) => {
   const { image_url, name, avg_score, reviews } = props.attributes
   const total = props.reviews.length
 
     return(
-        <div className='wrapper'>
+        <Wrapper>
           <h1><img src={image_url} alt={name} /> {name}</h1>
-          <div className='totalReviews'>{total} User Reviews </div>
+          <TotalReviews >{total} User Reviews </TotalReviews >
           <div className='starRatings'></div>
-          <div className='totalOutOf'>3 ou of 5</div>
-        </div>
+          <TotalOutOf>{avg_score}3 ou of 5</TotalOutOf>
+        </Wrapper>
     )
 
 }
